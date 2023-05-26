16165c4d2918f20d1f1a051fa1ed4833148454de
Sopanat Pongmarutai <tamsopanat123@gmail.com>
2023-05-26 15:11:44 +0700

    Update checkbox

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,6 @@ const addBtn = document.querySelector(".inputBlock button")
 const taskList = document.querySelector(".taskList")
 const delAllBtn = document.querySelector(".footer button")
 
-
-
-
 inputBox.onkeyup = () => {
     let userData = inputBox.value; //getting user entered value
     if (userData.trim() != 0) { //if user values aren't only spaces
@@ -41,19 +38,33 @@ function showTasks() {
 
     const taskNum = document.querySelector(".taskNum");
     taskNum.textContent = listArr.length; //passing the length value in taskNum
-    if (listArr.length > 0){ //if array is not empty
+    if (listArr.length > 0) { //if array is not empty
         delAllBtn.classList.add("active");
-    }else{
+    } else {
         delAllBtn.classList.remove("active")
     }
-    
+
     let newLiTag = '';
     listArr.forEach((element, index) => {
-        newLiTag += '<li>' + element + '<span onclick="deleteTask(' + index + ')"><i class="fas fa-trash"></i></span></li>';
+        newLiTag += '<li><input type="checkbox" class="checkbox"/><label><label>' + element + '</label><span onclick="deleteTask(' + index + ')"><i class="fas fa-trash"></i></span></li>';
     })
     taskList.innerHTML = newLiTag; //adding new li tag inside ul tag
     inputBox.value = ''; //once task added leave the input field blank
     addBtn.classList.remove("active")
+
+    const checkboxes = document.querySelectorAll('.checkbox');
+    console.log(checkboxes)
+    checkboxes.forEach((element) =>{
+        console.log(element)
+        element.addEventListener('change', function () {
+            const task = element.nextElementSibling;
+            if (element.checked) {
+                task.style.textDecoration = 'line-through';
+            } else {
+                task.style.textDecoration = 'none';
+            }
+        });
+    });
 }
 
 //delete task function
@@ -66,9 +77,10 @@ function deleteTask(index) {
     showTasks();
 }
 
+
 //delete all tasks button
-delAllBtn.onclick = ()=>{
-    listArr=[]; //empty an array
+delAllBtn.onclick = () => {
+    listArr = []; //empty an array
     //Update the local storage
     localStorage.setItem("New Todo", JSON.stringify(listArr));
     showTasks();
